8bcbe31144f0e905368def6533ab16daea954102
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 09:20:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,29 @@
-
 #include <iostream>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include <crypt.h>
-#include <iomanip>
-#include <fstream>
 using namespace std;
 
-void findPass(string pass, const string& hash);
-
-int main(int argc, char* argv[])
+int main()
 {
-	int i = 0, j = 0;
-	ifstream f("/root/Laba8/Zadanie1/hashs");
-	f.seekg(0, ios::end);
-	int fsize = f.tellg();
-	f.seekg(0, ios::beg);
-	char *buf = new char [fsize];
-	char *save = new char[60];
-	f.read(buf, fsize);
-	for (i = 0; i < fsize; i++) {
-		if (buf[i] == '\n' || i == (fsize-1)) {
-			save[j] = '\0';
-			findPass("123456789", save);
-			delete[] save;
-			save = new char[60];
-			j = 0;
-		} else {
-			save[j] = buf[i];
-			++j;
-		}
+	int peremennaya, iskomoe;
+	cout << "Введите искомое число." << endl;
+	cin >> iskomoe;
+	if (iskomoe==0) {
+	cout<< "Ноль не может быть искомым"<<endl;
 	}
-	return 0;
-}
+	else{
 
-void findPass(string pass, const string& hash)
-{
-	crypt_data *pCryptData = new crypt_data;
-	size_t pos = hash.find_last_of('$');
-
-	string hashHead = hash.substr(0,pos);
 	do {
-		string newHash(crypt_r(pass.data(),hashHead.data(),pCryptData));
-		if (newHash == hash) {
-			cout<<"Hash: "<<hash<<endl<<"Pass: "<<pass<<endl;
+		cout << "Введите число"<< endl;
+		cin >> peremennaya;
+		if (peremennaya==iskomoe) {
+			cout << "Найдено искомое число"<< endl;
+		} else if (peremennaya==0) {
+			cout << "Возможно вы ввели ноль или не числовое значение" << endl;
 			break;
+		} else {
+			continue;
 		}
-	} while ( next_permutation( pass.begin(), pass.end() ) );
-	delete pCryptData;
+	} while (peremennaya!=iskomoe);
+	}
+return 0;
 }
+
